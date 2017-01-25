9bf392676cc90a370628790d1e5642413c4559a1
Jess Telford <jess+github@jes.st>
2017-01-26 00:16:31 +1100

    Added inputVCR + Started on scene

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+  // meters per second
+  public float scrollSpeed;
+
+  private bool isScrolling;
+
+  // Use this for initialization
+  void Start () {
+    isScrolling = false;
+  }
+
+  // Update is called once per frame
+  void Update () {
+    // after 2 seconds, start scrolling the camera
+    if (!isScrolling && Time.time > 2.0f) {
+      isScrolling = true;
+    }
+
+    if (isScrolling) {
+      transform.position += new Vector3(scrollSpeed * Time.deltaTime, 0, 0);
+    }
+  }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+  public float speed;
+
+  private Rigidbody2D body;
+
+  void Start() {
+    body = GetComponent<Rigidbody2D>();
+    body.constraints = RigidbodyConstraints2D.FreezeRotation;
+  }
+
+  void FixedUpdate() {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    /*
+    // Can't move further forward than 3/4 of the screen (ish)
+    if (moveHorizontal > 0 && gameObject.position.x > camera.GetCenter() + (camera.GetWidth() * 0.75)) {
+      moveHorizontal = 0;
+    }
+    */
+
+    Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+
+
+    body.AddForce(movement * speed);
+
+    if (Input.GetButtonDown("Fire1")) {
+      // TODO: Firing logic
+    }
+  }
+}
