e8c084ac6ffd5788bc082e94b686ec8b18f9a338
Jess Telford <jess+github@jes.st>
2017-01-26 15:28:26 +1100

    Firing bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+  // When the bullet leaves the area it's allowed to be in,
+  // we destroy it
+  void OnTriggerExit2D(Collider2D other) {
+    if (other.name == "Bullet Limit") {
+      Destroy(gameObject);
+    }
+  }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,33 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
   public float speed;
+  public Rigidbody2D bullet;
 
   private Rigidbody2D body;
 
   void Start() {
     body = GetComponent<Rigidbody2D>();
-    body.constraints = RigidbodyConstraints2D.FreezeRotation;
   }
 
-  void FixedUpdate() {
+  void movePlayer() {
     float moveHorizontal = Input.GetAxis("Horizontal");
     float moveVertical = Input.GetAxis("Vertical");
 
-    /*
-    // Can't move further forward than 3/4 of the screen (ish)
-    if (moveHorizontal > 0 && gameObject.position.x > camera.GetCenter() + (camera.GetWidth() * 0.75)) {
-      moveHorizontal = 0;
-    }
-    */
-
     Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-
-
     body.AddForce(movement * speed);
+  }
+
+  void fireBullet() {
+    Vector3 fireFrom = transform.Find("Gun Forward").position;
+    Rigidbody2D newBullet = (Rigidbody2D)Instantiate(bullet, fireFrom, Quaternion.identity);
+    newBullet.velocity = new Vector3(10, 0, 0);
+  }
+
+  void FixedUpdate() {
+
+    movePlayer();
 
     if (Input.GetButtonDown("Fire1")) {
-      // TODO: Firing logic
+      fireBullet();
     }
   }
 }
