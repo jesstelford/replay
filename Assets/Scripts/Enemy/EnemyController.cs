e16459582b6fb7f1a68ce369126866139ec1361b
Jess Telford <jess+github@jes.st>
2017-02-03 21:19:50 +1100

    Enemy prefabs with scripts

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using Unidux;
+
+public class EnemyController : MonoBehaviour {
+
+  public float speed;
+  public Rigidbody2D bullet;
+
+  void Start() {
+  }
+
+}
---
+++ Assets/Scripts/Enemy/EnemyRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using Unidux;
+
+public class EnemyRenderer : MonoBehaviour {
+
+  public void Awake() {
+    StateManager.Store.AddRenderer(state => {
+      Debug.Log("Enemy Renderer");
+    });
+  }
+}
